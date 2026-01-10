87b43a17a84fe440bdce6d5177f5884c80aaec09
Yunusemreyilandil <yunusemreyilandil61@hotmail.com>
2026-01-10 03:48:28 +0300

    Stamina Bar (UI)

--- Assets/Yunus/Scripts/FlyController.cs
+++ Assets/Yunus/Scripts/FlyController.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class BirdFlightController : MonoBehaviour
@@ -11,6 +13,21 @@ public class BirdFlightController : MonoBehaviour
     public float idleFloatAmplitude = 0.08f;
     public float idleFloatSpeed = 2f;
 
+    [Header("Stamina")]
+    public float maxStamina = 5f;
+    public float staminaDrainPerSecond = 1.5f;
+    public float staminaRegenPerSecond = 3f;
+
+    [Header("Stamina UI")]
+    [SerializeField] Image staminaFill;
+    public float staminaUISpeed = 6f;
+
+
+
+    public float boostSpeedMultiplier = 1.5f;
+    float currentStamina;
+    bool isBoosting;
+
     Rigidbody2D rb;
     Vector2 input;
 
@@ -18,7 +35,7 @@ public class BirdFlightController : MonoBehaviour
     Vector3 visualStartLocalPos;
 
     [SerializeField] Transform visual;
-    
+
     [SerializeField] SpriteRenderer sr;
     [SerializeField] Animator animator;
 
@@ -27,6 +44,8 @@ public class BirdFlightController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         animator = visual.GetComponent<Animator>();
         visualStartLocalPos = visual.localPosition;
+
+        currentStamina = maxStamina;
     }
 
     void Update()
@@ -36,11 +55,20 @@ public class BirdFlightController : MonoBehaviour
         input.Normalize();
 
         UpdateAnimator();
+        HandleStamina();
+        
     }
 
     void FixedUpdate()
     {
-        Vector2 targetVelocity = input * moveSpeed;
+        float finalSpeed = moveSpeed;
+
+        if (isBoosting)
+            finalSpeed *= boostSpeedMultiplier;
+
+        Vector2 targetVelocity = input * finalSpeed;
+
+      
 
         rb.linearVelocity = Vector2.Lerp(
             rb.linearVelocity,
@@ -50,44 +78,76 @@ public class BirdFlightController : MonoBehaviour
     }
 
     void LateUpdate()
-    {
-        HandleIdleVerticalFloat();
-    }
+        {
+            HandleIdleVerticalFloat();
+        }
 
-    void UpdateAnimator()
-    {
-        float absX = Mathf.Abs(input.x);
-        float absY = Mathf.Abs(input.y);
+        void UpdateAnimator()
+        {
 
-        animator.SetFloat("MoveX", input.x);
-        animator.SetFloat("MoveY", input.y);
-        animator.SetBool("Horizontal", Mathf.Abs(input.x) > Mathf.Abs(input.y));
+            animator.SetFloat("MoveX", input.x);
+            animator.SetFloat("MoveY", input.y);
+            animator.SetBool("Horizontal", Mathf.Abs(input.x) > Mathf.Abs(input.y));
 
-        if (input.x > 0.1f)
-            sr.flipX = false;
-        else if (input.x < -0.1f)
-            sr.flipX = true;
+            if (input.x > 0.1f)
+                sr.flipX = false;
+            else if (input.x < -0.1f)
+                sr.flipX = true;
 
-        // yön önceliği kodda
-        animator.SetBool("Horizontal", absX > absY);
+
+        animator.SetBool("Boost", isBoosting);
+
     }
 
     void HandleIdleVerticalFloat()
+        {
+            if (rb.linearVelocity.magnitude < 0.1f)
+            {
+                idleTime += Time.deltaTime;
+                float yOffset = Mathf.Sin(idleTime * idleFloatSpeed) * idleFloatAmplitude;
+                visual.localPosition = visualStartLocalPos + new Vector3(0, yOffset, 0);
+            }
+            else
+            {
+                idleTime = 0f;
+                visual.localPosition = Vector3.Lerp(
+                    visual.localPosition,
+                    visualStartLocalPos,
+                    10f * Time.deltaTime
+                );
+            }
+        }
+        void HandleStamina()
+        {
+            bool shiftPressed = Input.GetKey(KeyCode.LeftShift);
+            if (shiftPressed & currentStamina > 0f && input.magnitude > 0.1f)
+            {
+                isBoosting = true;
+                currentStamina -= staminaDrainPerSecond * Time.deltaTime;
+
+            }
+            else
+            {
+                isBoosting = false;
+                currentStamina += staminaRegenPerSecond * Time.deltaTime;
+            }
+
+            currentStamina = Mathf.Clamp(currentStamina, 0f, maxStamina);
+
+        UpdateStaminaUI();
+
+    }
+
+    void UpdateStaminaUI()
     {
-        if (rb.linearVelocity.magnitude < 0.1f)
-        {
-            idleTime += Time.deltaTime;
-            float yOffset = Mathf.Sin(idleTime * idleFloatSpeed) * idleFloatAmplitude;
-            visual.localPosition = visualStartLocalPos + new Vector3(0, yOffset, 0);
-        }
-        else
-        {
-            idleTime = 0f;
-            visual.localPosition = Vector3.Lerp(
-                visual.localPosition,
-                visualStartLocalPos,
-                10f * Time.deltaTime
-            );
-        }
+        if (staminaFill == null) return;
+
+        float target = currentStamina / maxStamina;
+        staminaFill.fillAmount = Mathf.Lerp(
+            staminaFill.fillAmount,
+            target,
+            staminaUISpeed * Time.deltaTime
+        );
     }
 }
+
